90417355ff178f550ba6c4a48f6a5facffc1ed8e
David Aranda Baltazar <adavid.abaltazar@gmail.com>
2025-07-25 01:31:33 -0600

    Pruebas de Log
    Se experimentan con los msgs de logs

--- ProyectoDemoEBAC/Assets/Scripts/Hola Mundo.cs
+++ ProyectoDemoEBAC/Assets/Scripts/Hola Mundo.cs
@@ -7,7 +7,10 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning("Tal vez BarryX");
+        print("Algo pasa");
+        Debug.Log("Sucede algo");
+        Debug.LogWarning("Tal vez hay algo malo");
+        Debug.LogError("Algo salió mal");
     }
 
     // Update is called once per frame
