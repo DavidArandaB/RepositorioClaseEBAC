36f3895bc0cd111132b30ca45ba56238eb452f52
David Aranda Baltazar <adavid.abaltazar@gmail.com>
2025-08-15 00:10:01 -0600

    Componentes
    Componentes generados en el video de ejemplo de orden de ejecución

--- ProyectoDemoEBAC/Assets/Scripts/Hola Mundo.cs
+++ ProyectoDemoEBAC/Assets/Scripts/Hola Mundo.cs
@@ -7,15 +7,32 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Algo pasa");
-        Debug.Log("Sucede algo");
-        Debug.LogWarning("Tal vez hay algo malo");
-        Debug.LogError("Algo salió mal");
+        //print("Algo pasa");
+        //Debug.Log("Sucede algo");
+        //Debug.LogWarning("Tal vez hay algo malo");
+        //Debug.LogError("Algo salió mal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde fixed Update Cada 50 Frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("El objeto ha sido deshabilitado");
     }
 }
